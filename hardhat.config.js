8c1bbb5a20c62f4e985a78dc9a7fe76d7e4eb8cf
poppingbean <hivefive.of.teenages@gmail.com>
2025-05-23 11:33:24 +0700

    Add checkInWithRegisterIfNeeded

--- hardhat.config.js
+++ hardhat.config.js
@@ -6,8 +6,10 @@ module.exports = {
   solidity: "0.8.28",
   networks: {
     base: {
-      url: `https://base-mainnet.infura.io/v3/${process.env.INFURA_API_KEY}`,
-      accounts: [`0x${process.env.PRIVATE_KEY}`]
+      url: process.env.RPC_URL,
+      accounts: [`0x${process.env.PRIVATE_KEY}`],
+      gas: "auto",
+      gasPrice: "auto"
     }
   },
   etherscan: {
